3a04bf879e7d602171cfcf9311c8743a077869cc
shubham.kesarwani <shubham.kesarwani35@gmail.com>
2019-07-22 11:40:37 +0530

    Sample Project Created

---
+++ EasySelectionPatientChatbot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+namespace EasySelectionPatientChatbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
