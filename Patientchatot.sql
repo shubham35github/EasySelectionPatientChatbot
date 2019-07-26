67a073a9393f2cb02ba4bff3b7ee931dd0742750
venkatesh.prasad <venkateshprasad29@gmail.com>
2019-07-26 12:50:41 +0530

    Excel Sheet and Sql file added

---
+++ Patientchatot.sql
@@ -0,0 +1,21 @@
+CREATE TABLE PatientMonitors(
+monitors_no int primary key,
+measurment varchar(50),
+touchscreen tinyint,
+category varchar(50),
+portablity_1 varchar(50),
+feature_1 varchar(50),
+size_1 varchar(50),
+feature_2 varchar(50),
+feature_3 varchar(50),
+portablity_2 tinyint,
+ante_intrapartum varchar(50),
+display_mode varchar(50),
+weights varchar(50),
+feature_4 varchar(50),
+invasive_bp tinyint,
+co2_measurment tinyint,
+nbp_measurment tinyint,
+speed varchar(50),
+storage_size varchar(50)
+)
