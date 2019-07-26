84643aa344ae5f8c04ed38ad5956d02551d0dade
shubham.kesarwani <shubham.kesarwani35@gmail.com>
2019-07-26 12:36:50 +0530

    Interface and implemented class created

---
+++ EasySelectionPatientChatbot/EasySelectionPatientChatbot.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySelectionPatientChatbot
+{
+    class EasySelectionPatientChatbot :IEasySelectionChatbot
+    {
+        void CreateDatabase()
+        {
+
+        }
+        string ReadDatabase()
+        {
+            return "";
+        }
+        void DisplayItems()
+        {
+
+        }
+        void ResetItems()
+        {
+
+        }
+        void AbortApplication()
+        {
+
+        }
+        void BackPropagation()
+        {
+
+        }
+
+        void IEasySelectionChatbot.CreateDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        string IEasySelectionChatbot.ReadDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IEasySelectionChatbot.DisplayItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IEasySelectionChatbot.ResetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IEasySelectionChatbot.AbortApplication()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IEasySelectionChatbot.BackPropagation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
